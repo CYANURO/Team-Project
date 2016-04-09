a614855305418c4aacc2d9ead1d2a88e2198b296
Joshua Bailey <thejoshuabailey@gmail.com>
2016-04-09 13:24:57 -0600

    Gradle Build for the Project.

---
+++ TeamProjectGame/src/main/java/game/GameGui.java
@@ -0,0 +1,13 @@
+package game;
+
+/** 
+ * Where Everything gets drawn to the screen. 
+ */
+
+public class GameGui {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		
+	}
+
+}
